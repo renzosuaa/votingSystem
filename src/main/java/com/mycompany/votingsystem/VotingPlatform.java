ad95dfe2bc4f949c4a120e21f87d2ee9d4882525
Dom <Dom@DESKTOP-6GUPJLQ>
2024-11-18 21:34:50 +0800

    Update MainVotingPlatform.java

--- src/main/java/com/mycompany/votingsystem/VotingPlatform.java
+++ src/main/java/com/mycompany/votingsystem/VotingPlatform.java
@@ -9,12 +9,12 @@ import java.awt.*;
 import java.awt.event.*;
 import static java.awt.image.ImageObserver.HEIGHT;
 
-public class VotingPlatform extends JFrame{
+public class VotingPlatform extends JFrame implements ActionListener {
     
-    private JLabel lblcandidatename,lblcandidateid,lblpres,lblVpres,lblsenators;
-    private JScrollPane mainscrollpane,innerscrollpane1,innerscrollpane2,innerscrollpane3;
-    private JPanel mainpanel;
-    private JButton btnsubmit,btnsignout;
+    private JLabel lblCandidatename,lblCandidateid,lblPres,lblVpres,lblSenators;
+    private JScrollPane Mainscrollpane,Innerscrollpane1,Innerscrollpane2,Innerscrollpane3;
+    private JPanel Mainpanel;
+    private JButton btnSubmit,btnSignOut;
 
     
     VotingPlatform(){
@@ -28,47 +28,47 @@ public class VotingPlatform extends JFrame{
             setResizable(false);
             setTitle("Voting Selection ");
             
-            mainpanel = new JPanel();
-            mainpanel.setLayout(null);
-            mainpanel.setBackground(Color.LIGHT_GRAY);
+            Mainpanel = new JPanel();
+            Mainpanel.setLayout(null);
+            Mainpanel.setBackground(Color.LIGHT_GRAY);
             
-            lblpres = new JLabel("President");
-            lblpres.setFont(new Font("Arial",Font.BOLD,15));
-            lblpres.setBounds(30,20,200,30);
-            mainpanel.add(lblpres);
+            lblPres = new JLabel("President");
+            lblPres.setFont(new Font("Arial",Font.BOLD,15));
+            lblPres.setBounds(30,20,200,30);
+            Mainpanel.add(lblPres);
             
             lblVpres = new JLabel("Vice President ");
             lblVpres.setFont(new Font("Arial",Font.BOLD,15));
             lblVpres.setBounds(30,135,200,30);
            
-            mainpanel.add(lblVpres);
+            Mainpanel.add(lblVpres);
             
-            lblsenators = new JLabel("Senators ");
-            lblsenators.setFont(new Font("Arial",Font.BOLD,15));
-            lblsenators.setBounds(30,260,200,30);
+            lblSenators = new JLabel("Senators ");
+            lblSenators.setFont(new Font("Arial",Font.BOLD,15));
+            lblSenators.setBounds(30,260,200,30);
             
-            mainpanel.add(lblsenators);
+            Mainpanel.add(lblSenators);
            
             //scrollpane
             
-            innerscrollpane1 = new JScrollPane(mainscrollpane);
-            innerscrollpane1.setBounds(100,156,550,70);
-            add(innerscrollpane1);
+            Innerscrollpane1 = new JScrollPane(Mainscrollpane);
+            Innerscrollpane1.setBounds(100,156,550,70);
+            add(Innerscrollpane1);
             
-            innerscrollpane2 = new JScrollPane(mainscrollpane);
-            innerscrollpane2.setBounds(100,268,550,80);
-            add(innerscrollpane2);
+            Innerscrollpane2 = new JScrollPane(Mainscrollpane);
+            Innerscrollpane2.setBounds(100,268,550,80);
+            add(Innerscrollpane2);
             
-            innerscrollpane3 = new JScrollPane(mainscrollpane);
-            innerscrollpane3.setBounds(100,390,553,85);
-            add(innerscrollpane3);
+            Innerscrollpane3 = new JScrollPane(Mainscrollpane);
+            Innerscrollpane3.setBounds(100,390,553,85);
+            add(Innerscrollpane3);
             
             
-            mainscrollpane = new JScrollPane(mainpanel);
-            mainscrollpane.setBounds(50,100, 680, 400);
+            Mainscrollpane = new JScrollPane(Mainpanel);
+            Mainscrollpane.setBounds(50,100, 680, 400);
             //mainscrollpane.setBackground(Color.GRAY);
             //mainscrollpane.getViewport().setBackground(Color.LIGHT_GRAY);
-            add(mainscrollpane);
+            add(Mainscrollpane);
             
             
             
@@ -76,47 +76,48 @@ public class VotingPlatform extends JFrame{
             
             
             
-            lblcandidatename = new JLabel("Candidates Name");
-            lblcandidatename.setFont(new Font("Arial",Font.BOLD,25));
-            lblcandidatename.setBounds(30,30,700,30);
-            add(lblcandidatename);
+            lblCandidatename = new JLabel("Candidates Name");
+            lblCandidatename.setFont(new Font("Arial",Font.BOLD,25));
+            lblCandidatename.setBounds(30,30,700,30);
+            add(lblCandidatename);
             
-            lblcandidateid = new JLabel("Candidate_ID");
-            lblcandidateid.setFont(new Font("Arial",Font.BOLD,15));
-            lblcandidateid.setBounds(40,60,700,30);
-            add(lblcandidateid);
+            lblCandidatename = new JLabel("Candidate_ID");
+            lblCandidatename.setFont(new Font("Arial",Font.BOLD,15));
+            lblCandidatename.setBounds(40,60,700,30);
+            add(lblCandidatename);
             
             //button
             
             
-            btnsubmit = new JButton("SUBMIT");
-             btnsubmit.setBounds(450,515,120,30);
-            add( btnsubmit);
+            btnSubmit = new JButton("SUBMIT");
+             btnSubmit.setBounds(450,515,120,30);
+            add( btnSubmit);
             
-             btnsignout = new JButton("SIGN OUT");
-             btnsignout.setBounds(600,515,110,30);
-            add( btnsignout);
+             btnSignOut = new JButton("SIGN OUT");
+             btnSignOut.setBounds(600,515,110,30);
+            add( btnSignOut);
            
             //actionlistener for buttons
            
-           
-            this. btnsubmit.addActionListener(new ActionListener(){
-                @Override
-                    public void actionPerformed(ActionEvent e){
-                    
-                    if(e.getSource()==  btnsubmit){
-                        JOptionPane.showMessageDialog(null,"Your Vote is already submitted !","Voting Platform",JOptionPane.INFORMATION_MESSAGE);
-                    }else if(e.getSource()==  btnsignout){
-                            JOptionPane.showMessageDialog(null,"Thankyou for Voting !","Voting Platform", HEIGHT);
-                            }
-                    }
-            
-            });
-       
-           
-           
-            
+          
+            btnSubmit.addActionListener(this);
+            btnSignOut.addActionListener(this);
+    
+   
             setVisible(true);
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+             if (e.getSource() == btnSubmit) {
+            JOptionPane.showMessageDialog(this, "Your Vote is Already Submitted, Thank You !", "Voting Guide", JOptionPane.INFORMATION_MESSAGE);
+            dispose();
+        } else if (e.getSource() == btnSignOut) {
+            JOptionPane.showMessageDialog(this, "Thank You for Voting Wisely !", "Voting Guide", JOptionPane.INFORMATION_MESSAGE);
+            dispose();
+        }
+    }
+    
+    
     
 }
