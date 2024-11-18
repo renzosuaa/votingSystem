71fd57762b60f27a4007d0e323dcdcc0f3837020
Dom <Dom@DESKTOP-6GUPJLQ>
2024-11-18 20:33:40 +0800

    Update MainVotingPlatform.java

--- src/main/java/com/mycompany/votingsystem/VotingPlatform.java
+++ src/main/java/com/mycompany/votingsystem/VotingPlatform.java
@@ -11,17 +11,17 @@ import static java.awt.image.ImageObserver.HEIGHT;
 
 public class VotingPlatform extends JFrame{
     
-    private JLabel candidatename,candidateid,pres,Vpres,senators;
+    private JLabel lblcandidatename,lblcandidateid,lblpres,lblVpres,lblsenators;
     private JScrollPane mainscrollpane,innerscrollpane1,innerscrollpane2,innerscrollpane3;
     private JPanel mainpanel;
-    private JButton submit,signout;
+    private JButton btnsubmit,btnsignout;
 
     
     VotingPlatform(){
     
        
         
-            setSize(1200,700);
+            setSize(800,600);
             setLayout(null);
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             setLocationRelativeTo(null);
@@ -32,40 +32,40 @@ public class VotingPlatform extends JFrame{
             mainpanel.setLayout(null);
             mainpanel.setBackground(Color.LIGHT_GRAY);
             
-            pres = new JLabel("President");
-            pres.setFont(new Font("Arial",Font.BOLD,15));
-            pres.setBounds(50,30,200,30);
-            mainpanel.add(pres);
+            lblpres = new JLabel("President");
+            lblpres.setFont(new Font("Arial",Font.BOLD,15));
+            lblpres.setBounds(30,20,200,30);
+            mainpanel.add(lblpres);
             
-            Vpres = new JLabel("Vice President ");
-            Vpres.setFont(new Font("Arial",Font.BOLD,15));
-            Vpres.setBounds(50,150,200,30);
+            lblVpres = new JLabel("Vice President ");
+            lblVpres.setFont(new Font("Arial",Font.BOLD,15));
+            lblVpres.setBounds(30,135,200,30);
            
-            mainpanel.add(Vpres);
+            mainpanel.add(lblVpres);
             
-            senators = new JLabel("Senators ");
-            senators.setFont(new Font("Arial",Font.BOLD,15));
-            senators.setBounds(50,280,200,30);
+            lblsenators = new JLabel("Senators ");
+            lblsenators.setFont(new Font("Arial",Font.BOLD,15));
+            lblsenators.setBounds(30,260,200,30);
             
-            mainpanel.add(senators);
+            mainpanel.add(lblsenators);
            
             //scrollpane
             
             innerscrollpane1 = new JScrollPane(mainscrollpane);
-            innerscrollpane1.setBounds(100,170,900,70);
+            innerscrollpane1.setBounds(100,156,550,70);
             add(innerscrollpane1);
             
             innerscrollpane2 = new JScrollPane(mainscrollpane);
-            innerscrollpane2.setBounds(100,290,900,80);
+            innerscrollpane2.setBounds(100,268,550,80);
             add(innerscrollpane2);
             
             innerscrollpane3 = new JScrollPane(mainscrollpane);
-            innerscrollpane3.setBounds(100,420,900,150);
+            innerscrollpane3.setBounds(100,390,553,85);
             add(innerscrollpane3);
             
             
             mainscrollpane = new JScrollPane(mainpanel);
-            mainscrollpane.setBounds(50,100, 1050, 500);
+            mainscrollpane.setBounds(50,100, 680, 400);
             //mainscrollpane.setBackground(Color.GRAY);
             //mainscrollpane.getViewport().setBackground(Color.LIGHT_GRAY);
             add(mainscrollpane);
@@ -76,37 +76,37 @@ public class VotingPlatform extends JFrame{
             
             
             
-            candidatename = new JLabel("Candidates Name");
-            candidatename.setFont(new Font("Arial",Font.BOLD,25));
-            candidatename.setBounds(30,30,700,30);
-            add(candidatename);
+            lblcandidatename = new JLabel("Candidates Name");
+            lblcandidatename.setFont(new Font("Arial",Font.BOLD,25));
+            lblcandidatename.setBounds(30,30,700,30);
+            add(lblcandidatename);
             
-            candidateid = new JLabel("Candidate_ID");
-            candidateid.setFont(new Font("Arial",Font.BOLD,15));
-            candidateid.setBounds(40,60,700,30);
-            add(candidateid);
+            lblcandidateid = new JLabel("Candidate_ID");
+            lblcandidateid.setFont(new Font("Arial",Font.BOLD,15));
+            lblcandidateid.setBounds(40,60,700,30);
+            add(lblcandidateid);
             
             //button
             
             
-            submit = new JButton("SUBMIT");
-            submit.setBounds(700,610,120,30);
-            add(submit);
+            btnsubmit = new JButton("SUBMIT");
+             btnsubmit.setBounds(450,515,120,30);
+            add( btnsubmit);
             
-            signout = new JButton("SIGN OUT");
-            signout.setBounds(900,610,110,30);
-            add(signout);
+             btnsignout = new JButton("SIGN OUT");
+             btnsignout.setBounds(600,515,110,30);
+            add( btnsignout);
            
             //actionlistener for buttons
            
            
-            this.submit.addActionListener(new ActionListener(){
+            this. btnsubmit.addActionListener(new ActionListener(){
                 @Override
                     public void actionPerformed(ActionEvent e){
                     
-                    if(e.getSource()== submit){
+                    if(e.getSource()==  btnsubmit){
                         JOptionPane.showMessageDialog(null,"Your Vote is already submitted !","Voting Platform",JOptionPane.INFORMATION_MESSAGE);
-                    }else if(e.getSource()== signout){
+                    }else if(e.getSource()==  btnsignout){
                             JOptionPane.showMessageDialog(null,"Thankyou for Voting !","Voting Platform", HEIGHT);
                             }
                     }
